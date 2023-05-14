fef10de65fc256f3a1d53f80c601492c5be9b881
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-15 01:01:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,26 +2,20 @@
 function Person(name, age) {
 	this.name = name;
 	this.age = age;
-	this.greet = function(){
+	this.greet = ()=>{
 		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 	}
 }
 
-// Person.prototype.greet = function(){
-// 		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-// 	}
-
 function Employee(name, age, jobTitle) {
-	Person.call(this, name, age);
+	// Person.call(this, name, age);
 	this.jobTitle = jobTitle;
-	this.jobGreet = function(){
+	this.jobGreet = ()=>{
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
 	}
 }
 Employee.prototype = Object.create(Person.prototype);
-// Employee.prototype.jobGreet = function(){
-// 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
-// }
+
 
 // Do not change code below this line
 window.Person = Person;
