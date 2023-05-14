c38a8440bd4452cd6192a8834587c4252a374a91
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-14 17:04:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,22 @@
 function Person(name, age) {
 	this.name = name;
 	this.age = age;
-	this.greet = function(){
+	
+}
+
+Person.prototype.greet = function(){
 		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 	}
-}
 
 function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
 	this.jobTitle = jobTitle;
-	this.jobGreet = function(){
+	
+}
+
+Employee.prototype.jobGreet = function(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
 	}
-}
 Employee.prototype = Object.create(Person.prototype);
 
 const person = new Person("Alice", 25); 
