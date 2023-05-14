c8a6ecaf748f7313869613fa9046dbbc5f301859
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-15 01:13:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function Employee(name, age, jobTitle) {
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
 	}
 }
-Employee.prototype = Object.create(Person.prototype);
+// Employee.prototype = Object.create(Person.prototype);
 
 
 // Do not change code below this line
