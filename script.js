cfb5cda0eb74b6edb3dfbb8c687a047baaaef5e5
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-14 14:24:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ Employee.prototype.jobGreet = function(){
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
 }
 
-const person = new Person("Alice", 25); 
-person.greet();
-const employee = new Employee("Bob", 30, "Manager")
-employee.jobGreet();
+// const person = new Person("Alice", 25); 
+// person.greet();
+// const employee = new Employee("Bob", 30, "Manager")
+// employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
