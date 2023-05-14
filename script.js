db189c7d0bfa0506c51bda5e3249e2c755bad1f2
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-14 14:43:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function Employee(name, age, jobTitle) {
 Employee.prototype = Object.create(Person.prototype);
 
 Employee.prototype.jobGreet = function(){
+	Employee.greet();
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
 }
 
